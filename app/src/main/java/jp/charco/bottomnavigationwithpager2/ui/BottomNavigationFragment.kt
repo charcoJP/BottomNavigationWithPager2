d0d1c8b9ff8ebbc0ce3a585446bba788dcd8eca8
charcoJP <charco2356@gmail.com>
2022-07-04 22:45:37 +0900

    ViewPager2 で下タブのFragmentを管理

--- app/src/main/java/jp/charco/bottomnavigationwithpager2/ui/BottomNavigationFragment.kt
+++ app/src/main/java/jp/charco/bottomnavigationwithpager2/ui/BottomNavigationFragment.kt
@@ -5,7 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import jp.charco.bottomnavigationwithpager2.R
 import jp.charco.bottomnavigationwithpager2.databinding.FragmentBottomNavBinding
+import jp.charco.bottomnavigationwithpager2.ui.dashboard.DashboardFragment
+import jp.charco.bottomnavigationwithpager2.ui.home.HomeFragment
+import jp.charco.bottomnavigationwithpager2.ui.notifications.NotificationsFragment
 
 class BottomNavigationFragment : Fragment() {
 
@@ -21,8 +27,41 @@ class BottomNavigationFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.pager.adapter = BottomNavigationPagerAdapter(requireActivity())
+        binding.pager.isUserInputEnabled = false
+
+        binding.navView.setOnItemSelectedListener {
+            val currentItem =
+                when(it.itemId) {
+                    R.id.navigation_home -> 0
+                    R.id.navigation_dashboard -> 1
+                    else -> 2
+                }
+            binding.pager.setCurrentItem(currentItem, false)
+            return@setOnItemSelectedListener true
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+}
+
+
+class BottomNavigationPagerAdapter(fm: FragmentActivity) : FragmentStateAdapter(fm) {
+    override fun getItemCount(): Int {
+        return 3
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return when(position) {
+            0 -> HomeFragment()
+            1 -> DashboardFragment()
+            else -> NotificationsFragment()
+        }
     }
 }
